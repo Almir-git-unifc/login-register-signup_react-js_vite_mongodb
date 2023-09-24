7b6ef2b30ba63aa16401a2bd34341c211655e5e4
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2023-09-24 17:22:30 -0300

    feat(react): v0.01.000 - create front-end register-signup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './componets/Signup';
+
+function App() {
+
+  return (
+      <div>
+         <p>Paragrafo Temporário do arquivo App.jsx, enquanto não chega outros códigos</p>
+         <Signup />
+      </div>
+  )
+}
+
+export default App
