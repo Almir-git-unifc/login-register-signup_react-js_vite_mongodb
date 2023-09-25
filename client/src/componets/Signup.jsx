10ecae1d156c47bf8e607c0c47057331f424c63f
Silvinsk <81040770+Almir-git-unifc@users.noreply.github.com>
2023-09-24 22:06:04 -0300

    feat(react): v00.03.000 Front end can now register a new user in MongoDB using the Signup.jsx file

--- client/src/componets/Signup.jsx
+++ client/src/componets/Signup.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import '../App'
+import '../App';
+import axios from "axios";
 
 function Signup () {
 
   const navigate = useNavigate();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
 
   const handleClick = () => {
     navigate("/login");
   };
+
+  /** AQUI PASSAREMOS OS DADOS RECEBIDOS NO CLIENT PARA O BACKEND */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Usaremos o axios para enviar os dados
+    // Daqui vai para a rota, e depois vai inserir as informações no Banco de Dados
+    //{name, email, password}  = {name: name, email: email, password: password} 
+    axios.post("http://localhost:3005/register", {name, email, password} )
+    .then(result => { console.log(result) 
+      navigate("/login");
+    })
+    .catch(err => console.log(err) )
+ }
+    // Como chamamos a rota register axios.post("http://localhost:3005/register"
+    // Então vamos ter que criar esta rota /register   no arquivo index.js
 
   return (
     <div
@@ -16,7 +37,7 @@ function Signup () {
     >
       <div className='bg-white p-3 rounded w-25'>
         <h2>Register Signup</h2>
-        <form>
+        <form onSubmit ={handleSubmit}>
           <div className='mb-3'>
             <label htmlFor='email'>
               <strong>Name</strong>
@@ -27,6 +48,7 @@ function Signup () {
               autoComplete='off'
               name='email'
               className='form-control rounded-0'
+              onChange={ (e) => setName(e.target.value) }
             />
           </div>
 
@@ -40,6 +62,7 @@ function Signup () {
               autoComplete='off'
               name='email'
               className='form-control rounded-0'
+              onChange={ (e) => setEmail(e.target.value) }
             />
           </div>
       
@@ -53,6 +76,7 @@ function Signup () {
               placeholder='Enter Password'
               name='password'
               className='form-control rounded-0'
+              onChange={ (e) => setPassword(e.target.value) }
             />
           </div>
    
